c44a51ca8aef98cf1414b217c6607f4eae452750
Yuka Sakaguchi <sakaguchi.pg@gmail.com>
2015-02-08 22:44:16 +0900

    三角形のポリゴン表示

--- TestOpenGL/app/src/main/java/jp/ysakaguchi/testopengl/MainActivity.java
+++ TestOpenGL/app/src/main/java/jp/ysakaguchi/testopengl/MainActivity.java
@@ -1,39 +1,58 @@
 package jp.ysakaguchi.testopengl;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    private GLSurfaceView mGLSurfaceView;
+    private boolean isSupportOpenGl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        isSupportOpenGl = configurationInfo.reqGlEsVersion >= 0x20000; // 端末がOpenGL ES 2.0をサポートしているかチェック
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (!isSupportOpenGl) {
+            final RelativeLayout relativeLayout = new RelativeLayout(this);
+            final TextView tv = new TextView(this);
+            tv.setText("端末がOpenGL ES 2.0に対応していません。");
+            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+            relativeLayout.addView(tv, lp);
+            setContentView(relativeLayout);
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
+        mGLSurfaceView = new GLSurfaceView(this);
+        mGLSurfaceView.setEGLContextClientVersion(2);  // OpenGLバージョンを設定
+        mGLSurfaceView.setRenderer(new MyRenderer());  // レンダラを設定
+        setContentView(mGLSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isSupportOpenGl && null != mGLSurfaceView) {
+            mGLSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isSupportOpenGl && null != mGLSurfaceView) {
+            mGLSurfaceView.onPause();
+        }
     }
 }
